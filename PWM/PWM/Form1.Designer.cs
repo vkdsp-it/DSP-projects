97e3287030505bc048c6d88b1dbfb336e0e94f18
vkdsp-it <97973643+vkdsp-it@users.noreply.github.com>
2022-01-18 22:10:38 +0300

    Add files via upload

---
+++ PWM/PWM/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace PWM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.AntiAliasing = System.Windows.Forms.DataVisualization.Charting.AntiAliasingStyles.None;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Sine";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Carrier";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "PWM";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(689, 435);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(284, 527);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Do PWM";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(795, 526);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 24);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(759, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(47, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Sine";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(759, 49);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(56, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Carrier";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(759, 84);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(53, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "PWM";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(770, 118);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 183);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(770, 328);
+            this.trackBar2.Maximum = 50;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 172);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 562);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.chart1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ PWM/PWM/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet;
+using MathNet.Numerics;
+
+namespace PWM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        #region Define Variables
+        static double freqSine = 50.0;
+        static double freqCarrier = 33.0 * freqSine;
+        static double sine2A;
+        static double carrierA;
+
+        static double simTimeCycles = 1.0;
+        static double simTimeSeconds = simTimeCycles / freqSine;
+        static int numSamples = 10000;
+        static int samplesPerSecond = (int)(numSamples / simTimeSeconds);
+
+        //  Triangle carrier period in seconds
+        static double carrierPeriodSeconds = 1.0 / freqCarrier;
+
+        //  Triangle carrier period in samples
+        static int carrierPeriodSamples = (int)(carrierPeriodSeconds * samplesPerSecond);
+
+        //  Plot parameters
+        static int plotCyclesFund = 1;
+        static double plotTime = plotCyclesFund / freqSine;
+        //static int plotSamples = plotCyclesFund;
+        #endregion
+
+        #region Define Arrays
+        double [] sine = new double[numSamples];
+        double [] sine2 = new double[numSamples];
+        double [] carrier = new double[numSamples];
+        double [] t = new double[numSamples];
+        double [] pwm = new double[numSamples];
+        #endregion
+
+        public void GeneratePWM()
+        {
+            #region Calculate Waveform Amplitudes from Trackbar Values
+            sine2A = trackBar1.Value * 0.1;
+            carrierA = 1 + trackBar2.Value * 0.1;
+            #endregion
+
+            #region Generate Waveforms with Mathnet
+            sine = Generate.Sinusoidal(numSamples, samplesPerSecond, freqSine, 1.0);
+            sine2 = Generate.Sinusoidal(numSamples, samplesPerSecond, 2 * freqSine, sine2A);
+            carrier = Generate.Triangle(numSamples, carrierPeriodSamples / 2, carrierPeriodSamples / 2, -carrierA, carrierA);
+            #endregion
+
+            #region Prepare Chart
+            chart1.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
+            chart1.ChartAreas["ChartArea1"].AxisX.Maximum = plotTime;
+            chart1.ChartAreas["ChartArea1"].AxisX.RoundAxisValues();
+            chart1.ChartAreas["ChartArea1"].AxisX.LabelStyle.Format = "F4";
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineColor = Color.LightBlue;
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineColor = Color.LightBlue;
+            #endregion
+
+            #region Calculate PWM
+            for (int i = 0; i < numSamples; i++)
+            {
+                sine[i] += sine2[i];
+                t[i] = i / (double)samplesPerSecond;
+
+                if (sine[i] > carrier[i])
+                {
+                    pwm[i] = 1.0;
+                }
+                else pwm[i] = 0;
+            }
+            #endregion
+
+
+            PlotIt();
+
+        }
+
+
+
+
+
+        public void PlotIt()
+        {
+            chart1.Series["Sine"].Points.Clear();
+            chart1.Series["Carrier"].Points.Clear();
+            chart1.Series["PWM"].Points.Clear();
+
+            for (int i = 0; i < numSamples; i++)
+            {
+                if (checkBox1.Checked == true)
+                {
+                    chart1.Series["Sine"].Points.AddXY(t[i], sine[i]);
+                }
+
+                if (checkBox2.Checked == true)
+                {
+                    chart1.Series["Carrier"].Points.AddXY(t[i], carrier[i]);
+                }
+
+                if (checkBox3.Checked == true)
+                {
+                    chart1.Series["PWM"].Points.AddXY(t[i], pwm[i]);
+                }
+
+            }
+        }
+
+
+        #region Event Handlers
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            GeneratePWM();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            GeneratePWM();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            GeneratePWM();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            PlotIt();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            PlotIt();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            PlotIt();
+        }
+
+
+        #endregion
+
+
+    }
+}
